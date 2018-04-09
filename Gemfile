0f26eaeea36d23a93adaef184e5dc1b04526c12e
vedabrat <vedabrat>
2018-04-09 19:59:38 -0400

    pages working! java, functions, styles, and backend next!

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'rails_helper'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'rand#home'
+  get 'rand/_active'
+  get 'rand/_logout'
+  get 'rand/_scan'
+  get 'rand/_help'
+  get 'rand/_nav'
+  get 'rand/_search'
+  get 'rand/home'
+  get 'rand/_notifications'
+  get 'rand/_settings'
+  get 'rand/_incomplete'
+  get 'rand/_profile'
+  get 'rand/_invite'
+  get 'rand/_purchases'
+  get 'rand/_transfer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
